904ada5dba2beb4d52df18dccfd7511e6f4c20f1
Nathalia-Urenha <naty.urenha@gmail.com>
2021-11-03 23:27:28 -0300

    TypeScript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,22 +7,36 @@ import {
     Platform,
     FlatList,
     
-} 
-    from 'react-native';
+} from 'react-native';
 
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData{
+    id: string;
+    name: string;
+}
 
 export function Home(){
     const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState([]);
+    const [mySkills, setMySkills] = useState<SkillData[]>([]);
     const [greeting, setGretting] = useState('');
 
     function handleAddNewSkill(){
-        setMySkills(oldState => [... oldState, newSkill]);
+        const data = {
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+        setMySkills(oldState => [... oldState, data]);
     }
 
+    
+    function handleRemoveSkill(id: string){
+        setMySkills(oldState => oldState.filter(
+            skill => skill.id !== id
+        ));
+    }
+    
     useEffect(() => {
         const currentHour = new Date().getHours();
         if(currentHour < 12 && currentHour > 4){
@@ -53,7 +67,10 @@ export function Home(){
             onChangeText={setNewSkill}
          />
 
-        <Button onPress={handleAddNewSkill} />
+        <Button 
+            title="Add"
+            onPress={handleAddNewSkill} 
+        />
 
 
          <Text style={[styles.title, {marginVertical: 50}]}>
@@ -62,15 +79,18 @@ export function Home(){
          
          <FlatList 
             data={mySkills}
-            keyExtractor={item => item}
+            keyExtractor={item => item.id}
             renderItem={({item}) => (
-                <SkillCard skill={item}/>
+                <SkillCard 
+                    skill={item.name}
+                    onPress={() => handleRemoveSkill(item.id)}
+                />
             )}
          /> 
     </View>
     
   )
-}
+}''
 
 const styles = StyleSheet.create({
     container:{
